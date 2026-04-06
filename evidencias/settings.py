7462b538e90fb818ea7aec80e9230a107fcf71eb
gchernandezh <gchernandezh@gmail.com>
2026-04-06 11:46:07 -0500

    cambio a impresión por consola del correo, ya que por smtp no se puede por render.

--- evidencias/settings.py
+++ evidencias/settings.py
@@ -190,7 +190,8 @@ STORAGES = {
 }
 
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+#EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
