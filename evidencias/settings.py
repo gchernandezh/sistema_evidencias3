c146bcd891c4118d3534c3ca571c20739388494e
gchernandezh <gchernandezh@gmail.com>
2026-04-07 08:47:26 -0500

    ajuste de envio de correo con Brevo.

--- evidencias/settings.py
+++ evidencias/settings.py
@@ -189,12 +189,13 @@ STORAGES = {
     },
 }
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-#EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = "smtp-relay.brevo.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.environ.get("BREVO_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("BREVO_API_KEY")
+
+DEFAULT_FROM_EMAIL = os.environ.get("BREVO_USER")
